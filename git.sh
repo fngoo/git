4427d788ca49f5d8748fc58b53d64971af6af05d
fngoo <38164287+fngoo@users.noreply.github.com>
2019-10-02 20:00:51 +0800

    Update git.sh

--- git.sh
+++ git.sh
@@ -15,7 +15,7 @@ echo "cp -r /root/script/git/GSIL/* dir_$i" >> dir_$i/${i}.sh
 echo "cd dir_$i" >> dir_$i/${i}.sh
 echo "sed -e \"s/dot/${dot}/g\" rules.gsil > 1.txt ; mv 1.txt rules.gsil" >> dir_$i/${i}.sh
 echo "sed -e \"s/test/${line}/g\" rules.gsil > 1.txt ; mv 1.txt rules.gsil" >> dir_$i/${i}.sh
-echo "python3 gsil.py ${line}" >> dir_$i/${i}.sh
+echo "python3 gsil.py ${dot}" >> dir_$i/${i}.sh
 echo "cd ../ ; rm -r dir_$i" >> dir_$i/${i}.sh
 echo "bash dir_$i/${i}.sh" >> exe.sh
 i=$((i+1))
