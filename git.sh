64ffd74278315fcd4eaeae44da5592cc023e5fce
fngoo <38164287+fngoo@users.noreply.github.com>
2019-10-25 22:37:34 +0800

    Update git.sh

--- git.sh
+++ git.sh
@@ -38,7 +38,7 @@ echo ' ' >> $output/git_hog.txt
 echo "${line}" >> $output/git_hog.txt
 echo ' ' >> $output/git_hog.txt
 
-trufflehog ${line} --regex --entropy=False --max_depth 20 >> $output/git_hog.txt
+trufflehog ${line} --regex --entropy=False >> $output/git_hog.txt
 
 echo ' ' >> $output/git_hog.txt
 echo ' ' >> $output/git_hog.txt
