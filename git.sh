0a366e383f704d3b6a3ecf24aabfcc69ce4d6298
fngoo <38164287+fngoo@users.noreply.github.com>
2019-10-28 07:17:51 +0800

    Update git.sh

--- git.sh
+++ git.sh
@@ -35,7 +35,7 @@ echo ' ' >> $output/git_hog.txt
 echo "${line}" >> $output/git_hog.txt
 echo ' ' >> $output/git_hog.txt
 
-trufflehog ${line} --regex --entropy=False --max_depth 20 >> $output/git_hog.txt
+echo "trufflehog ${line} --regex --entropy=False >> $output/git_hog.txt" > 1.sh ; timeout 666 bash 1.sh ; rm 1.sh
 trufflehog --cleanup ${line}
 echo ' ' >> $output/git_hog.txt
 echo ' ' >> $output/git_hog.txt
